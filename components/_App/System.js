cb671b83f84c017041d0f34d2bca6fee737d431f
pondkarun <pondkarun@gmail.com>
2021-08-27 22:40:01 +0700

    System

--- components/_App/System.js
+++ components/_App/System.js
@@ -1,12 +1,23 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Layout from './Layout'
 
 const System = ({ children }) => {
-    return (
 
+    const { user } = useSelector(({ user }) => user);
+    const route = useRouter()
+
+    useEffect(() => {
+        if (user.roles_id !== "8a97ac7b-01dc-4e06-81c2-8422dffa0ca2") {
+            route.push("/")
+        }
+    }, [user])
+
+    return (
         <Layout>
             {children}
         </Layout>
-
     )
 }
 
