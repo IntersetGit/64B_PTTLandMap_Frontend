ca4542e5e6c28fc7dc401448759a414b3c8d792f
pondkarun <pondkarun@gmail.com>
2021-08-27 19:04:00 +0700

    PTT-herder.png

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,14 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel="shortcut icon" href="/images/logo.png" />
+          <link rel="shortcut icon" href="/assets/images/PTT-herder.png" />
           <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
           <link rel="stylesheet" href="/assets/css/font-awesome.min.css" />
           <link rel="stylesheet" href="/assets/css/line-awesome.min.css" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300&display=swap" rel="stylesheet" />
-          <link rel="icon" type="image/png" href="/favicon.png"></link>
+          <link rel="icon" type="image/png" href="/assets/images/PTT-herder.png"></link>
         </Head>
         <body>
           <Main />
