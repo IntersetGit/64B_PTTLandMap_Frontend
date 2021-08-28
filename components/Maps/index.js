ffdbba519e0b64ca58230869422fbce789854bdf
pondkarun <pondkarun@gmail.com>
2021-08-28 23:22:38 +0700

    MapPage

---
+++ components/Maps/index.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const Maps = (props) => {
+
+    console.log('props.center :>> ', props.center);
+    const googlemap = useRef(null);
+
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: "AIzaSyArK9veHmyKP3QdYMPW1381JzFHqUwDg9U",
+            version: 'weekly',
+        });
+
+        let map;
+        loader.load().then(() => {
+            const google = window.google;
+            map = new google.maps.Map(googlemap.current, {
+                center: { lat: props.center && props.center.lat ? props.center.lat : 13.78, lng: props.center && props.center.lng ? props.center.lng : 100.55 },
+                zoom: props.zoom ?? 8,
+            });
+
+            google.maps.event.addListener(map, "mousemove", (event) => {
+                getLatLon(event)
+            });
+        });
+    });
+
+    /* get Lat Lon */
+    const getLatLon = (event) => {
+        let _lat = event.latLng.lat();
+        let _lng = event.latLng.lng();
+        let _utm = (new LatLng(_lat, _lng)).toUTMRef().toString().replace(/([0-9.]+) ([0-9.]+)/, '$1, $2');
+        $('#latLong').text(`${_lat} / ${_lng}`)
+        $('#utm').text(` [${_utm}] `)
+    }
+
+    return (
+        <>
+
+            <div className="map-info-area">
+                <div class="map-info-detail">
+                    <span>Lat/Long <span id="latLong" /> UTM <span id="utm" /></span>
+
+                </div>
+            </div>
+
+            <div id="map" ref={googlemap} />
+
+        </>
+    )
+}
+
+export default Maps
--- pages/map.js
+++ pages/map.js
@@ -1,56 +1,10 @@
 import Layout from '../components/_App/Layout'
-import { useEffect, useRef, useState } from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
-import { Drawer, Row, Col } from 'antd';
+import { useState } from 'react';
+import { Drawer, Col } from 'antd';
 import Head from 'next/head';
+import Maps from '../components/Maps'
 
-const mapPage = () => {
-
-    const googlemap = useRef(null);
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let labelIndex = 0;
-
-    useEffect(() => {
-        const loader = new Loader({
-            apiKey: "AIzaSyArK9veHmyKP3QdYMPW1381JzFHqUwDg9U",
-            version: 'weekly',
-        });
-        let map;
-        loader.load().then(() => {
-            const google = window.google;
-            map = new google.maps.Map(googlemap.current, {
-                center: { lat: 13.78, lng: 100.55 },
-                zoom: 8,
-            });
-
-            google.maps.event.addListener(map, "click", (event) => {
-                addMarker(event.latLng, map);
-            });
-
-            google.maps.event.addListener(map, "mousemove", (event) => {
-                getLatLon(event)
-            });
-        });
-    });
-
-    function addMarker(location, map) {
-        // Add the marker at the clicked location, and add the next-available label
-        // from the array of alphabetical characters.
-        new google.maps.Marker({
-            position: location,
-            label: labels[labelIndex++ % labels.length],
-            map: map,
-        });
-    }
-
-    function getLatLon(event) {
-        let _lat = event.latLng.lat();
-        let _lng = event.latLng.lng();
-        let _utm = (new LatLng(_lat, _lng)).toUTMRef().toString().replace(/([0-9.]+) ([0-9.]+)/, '$1, $2');
-        $('#latLong').text(`${_lat} / ${_lng}`)
-        $('#utm').text(` [${_utm}] `)
-    }
-
+const MapPage = () => {
 
 
     /*  */
@@ -64,13 +18,6 @@ const mapPage = () => {
 
             <div className="tools-group-layer">
                 <button className="btn btn-light btn-sm" onClick={() => setVisible(true)}><i className="fa fa-window-restore" /></button>
-            </div>
-
-            <div className="map-info-area">
-                <div class="map-info-detail">
-                    <span>Lat/Long <span id="latLong" /> UTM <span id="utm" /></span>
-                    
-                </div>
             </div>
 
             <div className="tools-map-cog">
@@ -106,7 +53,7 @@ const mapPage = () => {
                 </Col>
             </div>
 
-            <div id="map" ref={googlemap} />
+            <Maps center={{ lat: 13.78, lng: 100.55 }} zoom={8} />
 
             <Drawer
                 title="Create a new account"
@@ -124,4 +71,4 @@ const mapPage = () => {
     )
 }
 
-export default mapPage
+export default MapPage
