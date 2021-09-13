877bcceb0deeb50bce5bac7e409a6b0460a29b92
Sirintra-264226 <77528383+Sirintra-264226@users.noreply.github.com>
2021-09-13 18:30:39 +0700

    new

--- pages/settings/system/group-layer/index.js
+++ pages/settings/system/group-layer/index.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import System from "../../../../components/_App/System";
-import { RedoOutlined, UploadOutlined } from "@ant-design/icons";
-import { Table, Modal, Input, Row, Col, Button, Form, Upload } from "antd";
+import {  MoreOutlined,RedoOutlined } from "@ant-design/icons";
+import { Table, Modal, Input, Row, Col, Button, Form } from "antd";
 import Api from "../../../../util/Api";
 const { Search } = Input;
 
 const GroupLayerSystemPage = () => {
-  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
   const [data, setData] = useState([]);
@@ -19,6 +18,7 @@ const GroupLayerSystemPage = () => {
 
   const handleOk = () => {
     form.submit();
+    setIsModalVisible(false);
   };
 
   const handleCancel = () => {
@@ -26,11 +26,8 @@ const GroupLayerSystemPage = () => {
     form.resetFields();
   };
   const onFinish = async (value) => {
-    setLoading(true);
     Api.post("/masterdata/masLayers", { group_name: value.groupLayer })
       .then((data) => {
-        setIsModalVisible(false);
-        setLoading(false);
         reload();
       })
       .catch((error) => {
@@ -42,7 +39,6 @@ const GroupLayerSystemPage = () => {
     Api.post("/masterdata/getmasLayers")
       .then((data) => {
         setData(data.data.items);
-        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -57,30 +53,33 @@ const GroupLayerSystemPage = () => {
       },
     },
     {
-      title: "group Layer",
+      title: "Symbol",
+      dataIndex: "Symbaol",
+      sorter: (record1, record2) => {
+        return record1.Symbaol > record2.Symbaol;
+      },
+    },
+    {
+      title: "ชื่อ group",
       dataIndex: "group_name",
       sorter: (record1, record2) => {
         return record1.group_name > record2.group_name;
       },
     },
+    {
+      title: "จัดการ",
+      dataIndex: "id",
+      render: (id) => {
+        return <MoreOutlined />;
+      },
+    },
   ];
 
   const search = (value) => {
-    setLoading(true);
     Api.post("/masterdata/getmasLayers", { search: value }).then((data) => {
       setData(data.data.items);
-      setLoading(false);
     });
   };
-
-  const normFile = (e) => {
-    console.log("Upload event:", e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e && e.fileList;
-  };
-
   useEffect(() => {
     reload();
   }, []);
@@ -91,17 +90,9 @@ const GroupLayerSystemPage = () => {
         <title>จัดการ Group Layer</title>
       </Head>
       <System>
-        <Row
-          gutter={[10, 10]}
-          style={{
-            background: "white",
-            padding: "16px",
-            boxShadow:
-              " rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
-          }}
-        >
+        <Row gutter={[10, 10]} style={{ background: "white", padding: "16px" }}>
           <Col span={24}>
-            <h3 className="mb-4">จัดการผู้ใช้งานระบบ</h3>
+            <h3>จัดการ Group Layer</h3>
           </Col>
           <Col span={5}>
             <Search placeholder="input search text" onSearch={search} />
@@ -116,30 +107,23 @@ const GroupLayerSystemPage = () => {
             </Button>
           </Col>
           <Col span={3} offset={11}>
-            <Button
-              type="primary"
-              onClick={showModal}
-              style={{ float: "right" }}
-            >
+            <Button type="primary" onClick={showModal}>
               + เพิ่ม group
             </Button>
           </Col>
           <Col span={24}>
-            <div className="table-responsive">
-              <Table
-                loading={loading}
-                columns={columns}
-                dataSource={data}
-                pagination={{
-                  current: page,
-                  pageSize: pageSize,
-                  onChange: (page, pageSize) => {
-                    setPage(page);
-                    setPageSize(pageSize);
-                  },
-                }}
-              />
-            </div>
+            <Table
+              columns={columns}
+              dataSource={data}
+              pagination={{
+                current: page,
+                pageSize: pageSize,
+                onChange: (page, pageSize) => {
+                  setPage(page);
+                  setPageSize(pageSize);
+                },
+              }}
+            />
           </Col>
         </Row>
       </System>
@@ -158,20 +142,11 @@ const GroupLayerSystemPage = () => {
           <Form.Item
             name="groupLayer"
             label="Group Layer"
-            rules={[{ required: true }]}
+            rules={[
+              { required: true, message: "Please input your grouplayer!" },
+            ]}
           >
-            <Input placeholder="Group Layer" />
-          </Form.Item>
-          <Form.Item
-            name="symbol"
-            label="Symbol"
-            rules={[{ required: true }]}
-            extra="ขนาดที่ recommend 50x50 pixcel"
-            getValueFromEvent={normFile}
-          >
-            <Upload>
-              <Button icon={<UploadOutlined />}>Select File</Button>
-            </Upload>
+            <Input />
           </Form.Item>
         </Form>
       </Modal>
