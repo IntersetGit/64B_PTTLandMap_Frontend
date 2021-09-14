a2b44c9f3b62e9cca4aeeb404c6e7d57bc0204c7
yochi2020 <yochi.suz@gmail.com>
2021-09-14 18:00:26 +0700

    .

--- components/_App/Sidebar.js
+++ components/_App/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
     const activeHref = (path) => asPath.toUpperCase() === path.toUpperCase() ? "active" : "";
 
     return (
-        <div className="sidebar" id="sidebar">
+        <div className="sidebar" id="sidebar" style={{zIndex:"2"}}>
             <div className="sidebar-inner slimscroll">
                 <div id="sidebar-menu" className="sidebar-menu">
                     <ul>
@@ -16,7 +16,7 @@ function Sidebar() {
                         </li>
                         <li className={activeHref(`/settings/dashboard/report-summary/`)}>
                             <Link href={`/settings/dashboard/report-summary/`} >
-                                <a><i className="la la-pie-chart pb-2" /> <span >รายงานการสรุปผล <br /><h5 className="pt-2">การดำเนินงาน</h5></span></a>
+                                <a><img src="/assets/images/icon/4.png" className=" pb-2" alt=""  width={23}/> <span >รายงานการสรุปผล <br /><h5 className="pt-2">การดำเนินงาน</h5></span></a>
                             </Link>
                         </li>
                         {/* <li className={activeHref(`/settings/dashboard/`)}>
@@ -37,29 +37,29 @@ function Sidebar() {
 
                         <li className={activeHref(`/settings/system/users/`)}>
                             <Link href={`/settings/system/users/`} >
-                                <a><img src="/assets/images/icon/11.png" alt=""  width={25}/><span>จัดการผู้ใช้ระบบ</span></a>
+                                <a><img src="/assets/images/icon/11.png" className=" pb-2" alt=""  width={25}/><span>จัดการผู้ใช้ระบบ</span></a>
                             </Link>
                         </li>
 
                         <li className={activeHref(`/settings/system/group-layer/`)}>
                             <Link href={`/settings/system/group-layer/`} >
-                                <a><img src="/assets/images/icon/5.png" alt=""  width={25}/><span>จัดการ Group Layer</span></a>
+                                <a><img src="/assets/images/icon/5.png" className=" pb-2" alt=""  width={25}/><span>จัดการ Group Layer</span></a>
                             </Link>
                         </li>
 
                         <li className={activeHref(`/settings/system/control-gis-layer/`)}>
                             <Link href={`/settings/system/control-gis-layer/`} >
-                                <a><img src="/assets/images/icon/4.png" alt=""  width={23}/> <span >จัดการ ข้อมูล GIS Layer</span></a>
+                                <a><img src="/assets/images/icon/4.png" className=" pb-2" alt=""  width={23}/> <span >จัดการ ข้อมูล GIS Layer</span></a>
                             </Link>
                         </li>
                         <li className={activeHref(`/settings/system/satellite-aerial-photographs/`)}>
                             <Link href={`/settings/system/satellite-aerial-photographs/`} >
-                                <a><img src="/assets/images/icon/7.png" alt="" width={25}  /> <span >จัดการ ภาพถ่ายดาวเทียม<h5 className="pt-2">และภาพถ่ายทางอากาศ</h5></span></a>
+                                <a><img src="/assets/images/icon/7.png" className=" pb-2" alt="" width={25}  /> <span >จัดการ ภาพถ่ายดาวเทียม<h5 className="pt-2">และภาพถ่ายทางอากาศ</h5></span></a>
                             </Link>
                         </li>
                         <li className={activeHref(`/settings/system/control-status-project/`)}>
                             <Link href={`/settings/system/control-status-project/`} >
-                                <a><img src="/assets/images/icon/6.png" alt=""  width={24}/><span > จัดการ Status โครงการ</span></a>
+                                <a><img src="/assets/images/icon/6.png" className=" pb-2" alt=""  width={24}/><span > จัดการ Status โครงการ</span></a>
                             </Link>
                         </li>
 
