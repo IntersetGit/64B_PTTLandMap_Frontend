dc9b380bac5d9b246ae0fd1bf0079e864365892f
Thunwa007 <vathunwa@gmail.com>
2021-10-21 15:07:07 +0700

    update p wa

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Timeslider from '../components/Timeslider'
+
+function Test() {
+    return (
+        <div style={{ height: "100%", width: "100%" }}>
+            <Timeslider />
+        </div>
+    )
+}
+
+export default Test
