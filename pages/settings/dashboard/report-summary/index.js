d54bafc2cb3ae063336c09fcb5be20db384579d9
C.chanaphat <yochi.suz@gmail.com>
2021-10-27 11:53:15 +0700

    .

--- pages/settings/dashboard/report-summary/index.js
+++ pages/settings/dashboard/report-summary/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import System from '../../../../components/_App/System'
+import { Input, Col, Row, Select, Button, Table } from 'antd'
+const { Option } = Select;
+const { Column, ColumnGroup } = Table
 const index = () => {
     return (
         <>
@@ -8,6 +11,44 @@ const index = () => {
             </Head>
             <System>
                 <h3>รายงานการสรุปผลการดำเนินงาน</h3>
+                <Input.Group size="default">
+                    <Row gutter={8}>
+                        <Col span={3}>
+                            <Select defaultValue="lucy" style={{ width: "100%" }} >
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </Col>
+                        <Col span={3}>
+                            <Select defaultValue="lucy" style={{ width: "100%" }} >
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </Col>
+                        <Col span={3}>
+                            <Select defaultValue="lucy" style={{ width: "100%" }} >
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </Col>
+                        <Col span={3}>
+                            <Select defaultValue="lucy" style={{ width: "100%" }} >
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </Col>
+                        <Col span={3}>
+                            <Select defaultValue="lucy" style={{ width: "100%" }} >
+                                <Option value="lucy">Lucy</Option>
+                            </Select>
+                        </Col>
+                        <Col span={3}>
+                            <Button type="primary">Report</Button>
+                        </Col>
+                        <Col span={3}>
+                            <button className="btn btn-success" style={{ borderRadius: "2px" }}>Export To Excel</button>
+                        </Col>
+                    </Row>
+                </Input.Group>
+                <Table>
+
+                </Table>
             </System>
         </>
     )
